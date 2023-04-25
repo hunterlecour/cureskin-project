96031bf15192cfe1608d48927c7477f8969bff51
hunterlecour <hunterlecour@gmail.com>
2023-04-25 10:14:29 -0400

    Top logo takes to the main page

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -6,11 +6,26 @@ def open_main_page(context):
     context.app.main_page.open_main_page()
 
 
+@given('Open search results page')
+def open_search_result(context):
+    context.app.main_page.open_search_result()
+
+
 @when('Click terms of service')
 def click_tos(context):
     context.app.main_page.click_tos()
 
 
+@when('Click on CureSkin logo in the header')
+def click_logo_header(context):
+    context.app.main_page.click_logo_header()
+
+
 @then('Verify terms page opened')
 def verify_tos_opens(context):
     context.app.main_page.verify_tos_opens()
+
+
+@then('Verify user is taken to the main page')
+def verify_on_main_page(context):
+    context.app.main_page.verify_on_main_page()
--- features/tests/main_page_tests.feature
+++ features/tests/main_page_tests.feature
@@ -5,3 +5,8 @@ Feature: Main Page Tests
     Given Open main page
     When Click terms of service
     Then Verify terms page opened
+
+  Scenario: Top logo takes to the main page
+    Given Open search results page
+    When Click on CureSkin logo in the header
+    Then Verify user is taken to the main page
