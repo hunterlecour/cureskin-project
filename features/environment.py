e9cebba4d5dc7ff1e274e17669f3f344d80164d0
hunterlecour <hunterlecour@gmail.com>
2023-05-02 20:59:03 -0400

    Add a reporting tool

--- features/environment.py
+++ features/environment.py
@@ -24,35 +24,35 @@ def browser_init(context, test_name):
     #     service=service
     # )
 
-    # # FIREFOX DRIVER
-    # service = Firefox_Service(executable_path=GeckoDriverManager().install())
-    # # context.driver = webdriver.Firefox(service=service)
-    #
-    # # # HEADLESS MODE ####
-    # # # FIREFOX
-    # options = webdriver.FirefoxOptions()
-    # options.add_argument('--headless')
-    # context.driver = webdriver.Firefox(
-    #     options=options, service=service
-    # )
+    # FIREFOX DRIVER
+    service = Firefox_Service(executable_path=GeckoDriverManager().install())
+    # context.driver = webdriver.Firefox(service=service)
+
+    # # HEADLESS MODE ####
+    # # FIREFOX
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')
+    context.driver = webdriver.Firefox(
+        options=options, service=service
+    )
 
     # BROWSER STACK #
 
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    bs_user = 'hunterlecour_hWPdTv'
-    bs_key = 'pXs7ZT4NHsRbZkCJgmMV'
-
-    desired_cap = {
-        'browserName': 'Firefox',
-        'bstack:options': {
-            'os': 'OS X',
-            'osVersion': 'Ventura',
-            'sessionName': test_name
-        }
-    }
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
-
+    # bs_user = 'hunterlecour_hWPdTv'
+    # bs_key = 'pXs7ZT4NHsRbZkCJgmMV'
+    #
+    # desired_cap = {
+    #     'browserName': 'Firefox',
+    #     'bstack:options': {
+    #         'os': 'OS X',
+    #         'osVersion': 'Ventura',
+    #         'sessionName': test_name
+    #     }
+    # }
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+    #
     context.driver.maximize_window()
     context.driver.implicitly_wait(5)
     context.driver.wait = WebDriverWait(context.driver, 10)
